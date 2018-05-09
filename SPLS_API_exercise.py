433f400982d0f6d973aaa0612a94343ddc48466a
kushagraagarwal19 <kushagra.agarwal@outlook.com>
2018-05-09 13:05:16 -0400

    Initialized repo and removed unnecessary files

--- SPLS_API_exercise.py
+++ SPLS_API_exercise.py
@@ -2,14 +2,16 @@ from flask import request
 from flask import jsonify, make_response
 from flask import Flask
 import datetime
+
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+
+import csv
 
 # =====================================================
 
 
 def estimate_price(house_feat):
-
     price = 0.0
     price += 75.0*house_feat['GrLivArea']
     price += 54.0*house_feat['TotalBsmtSF']
@@ -17,7 +19,6 @@ def estimate_price(house_feat):
     price += 671.0*(house_feat['YearBuilt'] - 1990)
 
     return price
-
 # =====================================================
 
 # =====================================================
@@ -29,7 +30,6 @@ def checkFloat(s):
         return True
     except ValueError:
         return False
-
 # =====================================================
 
 
@@ -92,5 +92,78 @@ def houseprice():
 # =====================================================
 
 
+@app.route('/houselookup', methods=["GET"])
+def houselookup():
+    parameters = request.args
+
+    paramLength = len(parameters)
+    # Checking if parameters are odd or even and have at least 3 coordinate values
+    if paramLength >= 6 and paramLength % 2 != 0:
+        http_response = make_response(jsonify(
+            {'Error': 'To make a bounding box, coordinate values should be even in number'}
+        ))
+        return http_response
+
+    boundingBox = []
+
+    for i in range(0, paramLength//2):
+        dictKeyX = 'x' + str(i + 1)
+        dictKeyY = 'y' + str(i + 1)
+
+        if dictKeyX in parameters and dictKeyY in parameters:
+            if checkFloat(parameters[dictKeyX]) and checkFloat(parameters[dictKeyY]):
+                boundingBox.append(
+                    (float(parameters[dictKeyX]), float(parameters[dictKeyY])))
+                continue
+
+        http_response = make_response(jsonify(
+            {'Error': 'Please enter the paramaters in format x1, y1, x2, y2, x3, y3 .... and should be float values'}
+        ))
+        return http_response
+
+    # Create a polygon
+    polygon = Polygon(boundingBox)
+
+    # Now read CSV and check all the points exist or not in the polygon formed above
+    with open('house_coordinates.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        # Skip the header!
+        next(readCSV)
+
+        finalHouseIDs = []
+        i = 1
+        for row in readCSV:
+            try:
+                houseId = row[0]
+                x1 = float(row[1])
+                y1 = float(row[2])
+                x2 = float(row[3])
+                y2 = float(row[4])
+                x3 = float(row[5])
+                y3 = float(row[6])
+                x4 = float(row[7])
+                y4 = float(row[8])
+                point1 = Point(x1, y1)
+                point2 = Point(x2, y2)
+                point3 = Point(x3, y3)
+                point4 = Point(x4, y4)
+                i += 1
+
+                # Checking if the house is inside the polygon
+                if polygon.contains(point1) and polygon.contains(point2) and polygon.contains(point3) and polygon.contains(point4):
+                    finalHouseIDs.append(houseId)
+            except Exception as err:
+                print("Oops! An error occurred " + err)
+
+
+    http_response = make_response(jsonify(
+        {'HouseId': finalHouseIDs}
+    ))
+    return http_response
+
+# =====================================================
+
+# debug = True as it was in development mode.
+# For production, please remove this parameter
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
